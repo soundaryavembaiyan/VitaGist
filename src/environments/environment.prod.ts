33b77b1025c4d0cb861919ab85f9fb99bb53f3e6
Soundarya Vembaiyan <53108412+soundaryavembaiyan@users.noreply.github.com>
2023-04-05 11:14:28 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  xmppDomain: 'chat.digicoffer.com',
+  digicofferurl:'https://apidev2.digicoffer.com/cors/consumer/',
+  vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+
+export const environment = {
+  production: false,
+  digicofferurl:'https://apidev2.digicoffer.com/cors/consumer/',
+  vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
+  xmppDomain: 'chat.digicoffer.com',
+};
+
