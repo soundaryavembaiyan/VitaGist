bb45e3cc9334e515d4c2f3cb520a80b61fc0f721
Soundarya Vembaiyan <53108412+soundaryavembaiyan@users.noreply.github.com>
2023-12-15 09:21:43 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,6 @@ export const environment = {
   production: false,
   digicofferurl:'https://apidev2.digicoffer.com/cors/consumer/',
   vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
-  lateDoc:'https://devapi.latex.digicoffer.com',
   xmppDomain: 'chat.digicoffer.com',
 };
 
