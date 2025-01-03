31e53f567b071938e9db8cee8377ae890876239a
Soundarya Vembaiyan <soundaryavembaiyan@gmail.com>
2025-01-03 16:28:13 +0530

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -17,6 +17,24 @@ import {
 import { Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
+// import {
+//   FormGroupDirective,
+//   NgForm,
+//   Validators,
+//   FormsModule,
+//   ReactiveFormsModule,
+// } from '@angular/forms';
+// import {ErrorStateMatcher} from '@angular/material/core';
+// import {MatInputModule} from '@angular/material/input';
+// import {MatFormFieldModule} from '@angular/material/form-field';
+
+/** Error when invalid control is dirty, touched, or submitted. */
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+//   }
+// }
 
 @Component({
   selector: 'app-login',
@@ -64,7 +82,7 @@ submitData(data: any){
     this.router.navigate(['/side-bar'])
   }
   else{
-    this.toast.error("Check your login details")
+    this.toast.error("Please provide valid credentials")
   }
   })
 
