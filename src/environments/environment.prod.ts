f57906341a05cead562d2bf130372ffdb8d92ccb
soundarya <soundaryavembaiyan@gmail.com>
2025-01-07 16:45:26 +0530

    updated fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,3 @@ export const environment = {
   digicofferurl:'https://apidev2.digicoffer.com/cors/consumer/',
   vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
 };
-
-// export const environment = {
-//   production: true,
-//   xmppDomain: 'chat.digicoffer.com',
-//   digicofferurl:'https://apidev2.digicoffer.com/cors/consumer/',
-//   vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
-// };
