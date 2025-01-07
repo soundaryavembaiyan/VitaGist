c933abe9657814302785e370ee8198110be495ed
soundarya <soundaryavembaiyan@gmail.com>
2025-01-07 16:43:27 +0530

    unchanges

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,3 +4,10 @@ export const environment = {
   digicofferurl:'https://apidev2.digicoffer.com/cors/consumer/',
   vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
 };
+
+// export const environment = {
+//   production: true,
+//   xmppDomain: 'chat.digicoffer.com',
+//   digicofferurl:'https://apidev2.digicoffer.com/cors/consumer/',
+//   vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
+// };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -11,3 +11,15 @@ export const environment = {
     payment:"https://staging.payment.digicoffer.com/",
     appVersion: require('../../package.json').version + '-dev'
 };
+// export const environment = {
+//     production: false,
+//     cofferRestUrl: 'https://api.staging.digicoffer.com/',
+//     xmppDomain: 'devchat.vitacape.com',
+//     DOC2FILE :'https://dev.utils.doc2pdf.digicoffer.com/api/v1/docfile2pdf',
+//     DOC2PDF:'https://dev.utils.doc2pdf.digicoffer.com/api/v1/doc2pdf',
+//     cofferPersonalUrl: 'https://api.staging.digicoffer.com/consumer/sprelationship/',
+//     vitacapeRestUrl:'https://devapi.vitacape.digisecitus.com/api/v1/',
+//     AVChat:'https://staging.avchat.digicoffer.com/',
+//     payment:"https://staging.payment.digicoffer.com/",
+//     appVersion: require('../../package.json').version + '-dev'
+// };
